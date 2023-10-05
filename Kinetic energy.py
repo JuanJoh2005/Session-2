e4f565903f63aaed0bd7ba3191d6f7c6a89c0c39
JuanJoh2005 <145668230+JuanJoh2005@users.noreply.github.com>
2023-10-05 22:04:13 +0200

    Add files via upload

---
+++ Kinetic energy.py
@@ -0,0 +1,4 @@
+# Get user input for mass and velocity of the object
+mass = float(input("Enter the mass (in kilograms): "))
+velocity = float(input("Enter the velocity(in m/s): "))
+Kinetic Energy (KE) = 0.5 * mass * velocity^2
